630372b28aef108301092e42d1dda48c04a9d422
Katherine Manning <kat.m.manning94@gmail.com>
2020-10-02 13:01:18 -0400

    adding working app.js

--- Katherines Folder/app.js
+++ Katherines Folder/app.js
@@ -1,22 +1,31 @@
-d3.json("data/assault_us_array.json").then((data) => {
-    //  Create the Traces
-    var trace1 = {
-      x: data.yearOfRegistration,
-      y: data.gender,
-      type: "bar",
-      name: "TEST",
-      boxpoints: "all"
-    };
-  
-    // Create the data array for the plot
-    var data = [trace1];
-    let data2 = d3.json("data/assault_us_array.json")
-    console.log(data2)
-    // Define the plot layout
-    var layout = {
-      title: "TEST",
-    };
-  
-    // Plot the chart to a div tag with id "plot"
-    Plotly.newPlot("bar", data, layout);
-  });
+d3.json("data/assault_us_array.json").then((incomingData) => {
+	function filterAssault(assault) {
+		return assault.isSexualExploit == 1;
+	}
+
+	let filteredassault = incomingData.filter(filterAssault);
+
+	console.log(filteredassault)
+
+
+	let gender = filteredassault.map(gender => gender.gender);
+
+	let exploit = filteredassault.map(exploit => exploit.isSexualExploit);
+
+	console.log(exploit)
+
+
+	let trace = {
+		x: gender,
+		y: exploit,
+		type: "bar"
+	};
+
+	let data = [trace];
+
+	let layout = {
+		title: "testt"
+	}
+
+	Plotly.newPlot("bar",data,layout)
+})
