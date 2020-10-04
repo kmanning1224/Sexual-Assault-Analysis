db70eab170ee34505f24dbdc06106061f3990911
Kelsey Wyatt <kwyattkkg@gmail.com>
2020-10-04 14:29:53 -0400

    working on leaflet plot

---
+++ Kelsey/static/js/heatmap.js
@@ -0,0 +1,34 @@
+// Set up initial map center and zoom level
+var map = L.map('map', {
+  center: [40.7749, -97.4194], 
+  zoom: 4,  
+  scrollWheelZoom: true
+});
+
+var light = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+}).addTo(map); 
+
+
+var data = "static/us_trafficking_locations.json";
+
+d3.json(data, function(response) {
+
+  console.log(response);
+
+  var heatArray = [];
+
+  // for (var i = 0; i < response.length; i++) {
+  //   var location = response[i].location;
+
+  //   if (location) {
+  //     heatArray.push([location.coordinates[1], location.coordinates[0]]);
+  //   }
+  // }
+
+  var heat = L.heatLayer(heatArray, {
+    radius: 20,
+    blur: 35
+  }).addTo(map);
+
+});
--- Kelseys Folder/static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1Ijoia2x3MTFqIiwiYSI6ImNrZmVqZXZwdjA1ZTMycWxtNzFleG1lNTMifQ.NLpMj8I-pGYMKI6Q4hZSfA";
