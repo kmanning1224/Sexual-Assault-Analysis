933048a628b6a9134146baa6fe5a04572cb390ce
Katherine Manning <kat.m.manning94@gmail.com>
2020-10-05 18:10:17 -0400

    adding d3 script to html

--- templates/static/js/app.js
+++ templates/static/js/app.js
@@ -1,14 +1,9 @@
-function buildCharts(sample) {
-    var colorlist = ['#56dd3b','#3ad8bb','#3ba5e2','#3956db','#6639db',
-    '#9f38e0','#da36e2','#93226d','#701339','#510811','#443536','#1c1919']
-    // Fetch the sample data for the plots
-    var url = `/sqltest`;
-    d3.json(url).then(function(response) {
-      const gender = response.gender;
-      const year = response.yearOfRegistration;
-      const agerange = response.agerange;
-      const sexexploit = response.isSexualExploit;
-      console.log(response)
+d3.json("https://assaultdb.herokuapp.com/fulldate").then(function(response) {
+    const gender = response.gender;
+    const year = response.yearOfRegistration;
+    const agerange = response.agerange;
+    const sexexploit = response.isSexualExploit;
+    console.log(response)
   /*=========Build a bubble plot ==============*/
       var trace1 = {
         x: gender,
@@ -32,5 +27,3 @@ function buildCharts(sample) {
   
       Plotly.newPlot("bubble", data1, layout1)
     })
-};
-buildCharts();
