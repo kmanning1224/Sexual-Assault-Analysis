aea74a822045089ff43d93758a9fd8c6a5e33e36
Katherine Manning <kat.m.manning94@gmail.com>
2020-10-04 23:25:38 -0400

    updating heroku postgres pw

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
-dbuser = "vvyflfsgkzeqrz"
-dbpw = "4e33bb43a68544c07bd87cc445f24841a3e937d808ff9e3e6b98b7d9d303c313"
-dbhost = "ec2-54-197-254-117.compute-1.amazonaws.com"
+dbuser = "ozanjhjyyivzlm"
+dbpw = "39da857ae1301adae785280dc1a1da959a74a0614d75f83446b0bbcc96b3e2c0"
+dbhost = "ec2-34-231-56-78.compute-1.amazonaws.com"
 dbengine = "postgresql"
-db = "d7mkngu9oaro6h"
+db = "dcuvohmeofi05g"
 cxnstring = (os.environ["DATABASE_URL"] if os.getenv("DATABASE_URL")
 else f"{dbengine}://{dbuser}:{dbpw}@{dbhost}/{db}")
 # cxnstring = "sqlite:///assaultdb.sqlite"
