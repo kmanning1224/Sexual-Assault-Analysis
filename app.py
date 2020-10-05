a5a7af465fb5d6cad988c6f97b2df245e5282247
Katherine Manning <kat.m.manning94@gmail.com>
2020-10-05 16:19:33 -0400

    updating flask to link to db 2

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def psqltest():
 
 @app.route("/gender")
 def gender():
-    response = pd.read_sql("SELECT gender FROM assault_table_db WHERE yearOfRegistration = 2015")
+    response = pd.read_sql("SELECT gender FROM assault_per_state")
     return Response(response.to_json(orient = "records", date_format="iso"), mimetype="application/json")
 
 
