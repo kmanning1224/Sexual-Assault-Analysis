01d2e3fca8ae9e91f94e249a880a43ac780940a2
Katherine Manning <kat.m.manning94@gmail.com>
2020-10-04 21:03:16 -0400

    change database to heroku db

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
-dbuser = "postgres"
-dbpw = "postgres"
-dbhost = "localhost"
+dbuser = "vvyflfsgkzeqrz"
+dbpw = "4e33bb43a68544c07bd87cc445f24841a3e937d808ff9e3e6b98b7d9d303c313"
+dbhost = "ec2-54-197-254-117.compute-1.amazonaws.com"
 dbengine = "postgresql"
-db = "assault_db"
+db = "d7mkngu9oaro6h"
 cxnstring = (os.environ["DATABASE_URL"] if os.getenv("DATABASE_URL")
 else f"{dbengine}://{dbuser}:{dbpw}@{dbhost}/{db}")
-# cxnstring = "sqlite:///assaultdb.sqlite"
+cxnstring = "sqlite:///assaultdb.sqlite"
