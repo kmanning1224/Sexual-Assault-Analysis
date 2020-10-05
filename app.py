6dbc53a2a110173baa0f2871abee2b50bd8839f9
Katherine Manning <kat.m.manning94@gmail.com>
2020-10-05 01:14:20 -0400

    adding index.html base

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ engine = create_engine(cxnstring, pool_recycle=3600)
 
 @app.route("/")
 def index():
-    return "<h1> Deployed </h1>"
+    return render_template("index.html")
 
 @app.route("/sqltest")
 def psqltest():
