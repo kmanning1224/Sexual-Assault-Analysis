1c79db2f830bd9194c8c6ee99de2f4664168ace5
Katherine Manning <kat.m.manning94@gmail.com>
2020-10-05 12:47:33 -0400

    updating index.html

--- init_db.py
+++ init_db.py
@@ -3,4 +3,5 @@ from sqlalchemy import create_engine
 from config import cxnstring
 
 
-pd.read_csv('Data/Alltotals_global_dataset_2020.csv').to_sql(name="assault_table_db", con=create_engine(cxnstring), index=False)
+# pd.read_csv('Data/Alltotals_global_dataset_2020.csv').to_sql(name="assault_table_db", con=create_engine(cxnstring), index=False)
+pd.read_csv('Data/csvData.csv').to_sql(name="assault_per_state", con=create_engine(cxnstring), index=False)
