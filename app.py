3e01fb0e15a5dd0dde543a13b5b4277238a6e677
Katherine Manning <kat.m.manning94@gmail.com>
2020-10-06 00:52:16 -0400

    trying new mimetype for data pull

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, Response, render_template, jsonify
 import pandas as pd 
 from sqlalchemy import create_engine
 from config import cxnstring
+import requests
+import json
 app = Flask(__name__)
 
 engine = create_engine(cxnstring, pool_recycle=3600)
 # , pool_recycle=3600
-
 @app.route("/")
 def index():
     return render_template("home.html")
@@ -24,7 +25,8 @@ def gender():
 @app.route("/test")
 def test():
     response = pd.read_sql("SELECT * FROM assault_table_db", engine)
-    return jsonify(Response(response.to_json(orient = "records", date_format="iso"), mimetype="application/json"))
+    return Response(response.to_json(orient="records", date_format="iso"), mimetype="application/javascript")
+
 
 if __name__ == "__main__":
     app.run()
