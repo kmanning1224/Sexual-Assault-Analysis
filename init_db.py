cb9491b5834618f91412c384d17c635dbac35d5f
Katherine Manning <kat.m.manning94@gmail.com>
2020-10-06 17:03:09 -0400

    adding test db page

--- init_db.py
+++ init_db.py
@@ -11,3 +11,9 @@ df2 = pd.read_csv('Data/csvData.csv').reset_index(drop=True)
 df2 = pd.DataFrame(df2)
 df2.reset_index(drop=True)
 df2.to_sql(name="assault_per_state2", con=create_engine(cxnstring), if_exists="append")
+df3 = pd.read_csv('Data/new_global_dataset_2020.csv').reset_index(drop=True)
+df3 = pd.DataFrame(df2)
+df3.reset_index(drop=True)
+df3.to_sql(name="all_totals", con=create_engine(cxnstring), if_exists="append")
+
+
